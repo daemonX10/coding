250a17fdcaf1c96846d72aa97b984edfe67e1d0b
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-07-06 00:59:08 +0530

    javascript

---
+++ javascript/Array/array_day1.js
@@ -0,0 +1,16 @@
+let fruits=["apple","banana","cherry"];
+let last=fruits.pop();
+console.log(fruits);
+console.log(last);
+
+let fruits1=['apple','banana','mango'];
+let last1 =fruits1.push('cherry');
+console.log(fruits1);
+console.log(last1);
+
+// shift()
+
+let fruits3=['apple','banana','cherry','gavage'];
+let first3=fruits3.shift();
+console.log(fruits3);
+console.log(first3);
