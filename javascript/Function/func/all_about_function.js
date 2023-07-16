1ec3cd0c507f764a42fe669e5739ce7f8511b050
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-07-17 00:20:00 +0530

    javascript

---
+++ javascript/Function/func/all_about_function.js
@@ -0,0 +1,18 @@
+function damodar(){
+    console.log("money");
+}
+
+damodar();
+
+function math(a,b){
+    console.log(a+b);
+}
+
+math(2,5);
+
+let eren= function aot(paradise){ // here i'm declare
+return "titan";
+}
+
+let result = eren("Eldia");
+console.log(result); // Output: "titan"
